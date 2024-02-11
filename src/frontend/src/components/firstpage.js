5c4d34429e13c2f5ae61ac1883475401eb4bccc9
Benardo07 <117990908+Benardo07@users.noreply.github.com>
2024-02-12 02:59:15 +0700

    add rouer in the link

--- src/frontend/src/components/firstpage.js
+++ src/frontend/src/components/firstpage.js
@@ -12,8 +12,8 @@ function Firstpage() {
         <div className='border-y-4 border-solid w-2/3  border-[#d0ed57] shadow-[0_0_30px_0_rgba(208,237,87,1)] mt-5'></div>
 
         <div className='flex flex-row items-center justify-center gap-10 mt-10'>
-            <a> <button href="/api" className="border-2 border-[#d0ed57] border-solid p-4 text-xl font-sans font-semibold shadow-[5px_5px_0_0_rgba(208,237,87,1)] hover:bg-[#d0ed57] hover:bg-opacity-10 active:scale-95 transition duration-150 ease-in-out">Input Manual</button></a>
-            <a> <button className="border-2 border-[#d0ed57] border-solid p-4 text-xl font-sans font-semibold shadow-[5px_5px_0_0_rgba(208,237,87,1)] hover:bg-[#d0ed57] hover:bg-opacity-10 active:scale-95 transition duration-150 ease-in-out">Input From File</button></a>
+            <a href='/manual'> <button className="border-2 border-[#d0ed57] border-solid p-4 text-xl font-sans font-semibold shadow-[5px_5px_0_0_rgba(208,237,87,1)] hover:bg-[#d0ed57] hover:bg-opacity-10 active:scale-95 transition duration-150 ease-in-out">Input Manual</button></a>
+            <a href='/file-upload'> <button className="border-2 border-[#d0ed57] border-solid p-4 text-xl font-sans font-semibold shadow-[5px_5px_0_0_rgba(208,237,87,1)] hover:bg-[#d0ed57] hover:bg-opacity-10 active:scale-95 transition duration-150 ease-in-out">Input From File</button></a>
         </div>
       </div>
     );
