d8766601ea6c3807f5623263377bab67b301d228
Benardo07 <117990908+Benardo07@users.noreply.github.com>
2024-02-12 02:58:52 +0700

    feat : add modal pop up in manual input

--- src/frontend/src/components/manual.js
+++ src/frontend/src/components/manual.js
@@ -1,56 +1,158 @@
-import React from 'react'
+import React ,{useState} from 'react'
 
 function Manual() {
 
+
+    const [showResult, setShowResult] = useState(false);
+    const [results, setResults] = useState({});
+
+
+    const handleSubmit = async (event) => {
+        event.preventDefault(); // Prevent default form submission behavior
+
+        const formData = new FormData(event.target); // or new FormData(formRef.current)
+
+        // Log FormData values for debugging
+        for (let [key, value] of formData.entries()) {
+            console.log(`${key}: ${value}`);
+        }
+        try {
+            // Replace 'http://localhost:5000/solve' with your actual backend endpoint
+            const response = await fetch('/solve', {
+                method: 'POST',
+                body: formData,
+              });
+
+
+            const data = await response.json();
+            setResults(data);
+            setShowResult(true);
+        } catch (error) {
+            console.error('Error submitting form:', error);
+        }
+    };
+
+
+
+  const renderMatrix = (matrix) => {
+    return (
+      <table style={{ margin: "auto", borderCollapse: "collapse" }}>
+        <tbody>
+          {matrix.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {row.map((cell, cellIndex) => (
+                <td key={cellIndex} style={{ border: "1px solid #d0ed57", padding: "5px", textAlign: "center" }}>
+                  {cell}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  };
+  const handleResult = () => {
+    setShowResult(false);
+  }
   return (
     <div className='w-screen h-screen flex flex-col items-center  text-[#d0ed57] '>
-        <div className=" text-6xl font-sans font-bold text-shadow mt-20" >
-          Cyberpunk 2077 Breach Protocol
+        <div className={`flex flex-col items-center w-full h-full ${showResult ? 'filter blur-sm' : ''}`}>
+            <div className=" text-6xl font-sans font-bold text-shadow mt-20" >
+            Cyberpunk 2077 Breach Protocol
+            </div>
+            <form onSubmit={handleSubmit} className='grid grid-cols-2 mt-20 text-xl font-sans font-semibold w-1/2'>
+                <div className='p-5 flex flex-row justify-between'>
+                    <label >Jumlah Token Unik </label>
+                    <label >:</label>
+                </div>
+                <input type="text" name="uniqueToken" className='h-10 m-5 bg-transparent border-2 text-center shadow-[5px_5px_0_0_rgba(208,237,87,1)] p-3' required></input>
+                <div className='p-5 flex flex-row justify-between'>
+                    <label >Token Unik </label>
+                    <label >:</label>
+                </div>
+                <input type="text" name="token" className='h-10 m-5 bg-transparent border-2 text-center shadow-[5px_5px_0_0_rgba(208,237,87,1)]' required></input>
+                <div className='p-5 flex flex-row justify-between'>
+                    <label  >Ukuran Buffer </label>
+                    <label >:</label>
+                </div>
+                <input type="text" name="bufferSize" className='h-10 m-5 bg-transparent border-2 text-center shadow-[5px_5px_0_0_rgba(208,237,87,1)] 'required></input>
+                <div className='p-5 flex flex-row justify-between'>
+                    <label >Ukuran Matriks </label>
+                    <label >:</label>
+                </div>
+                <div className='flex flex-row p-5 gap-4'>
+                    <label className='mt-1' >Rows</label>
+                    <input type="text" name="matrixHeight" className='h-10  w-1/3 bg-transparent border-2 text-center shadow-[5px_5px_0_0_rgba(208,237,87,1)]' required></input>
+                    <label className='mt-1' >Cols</label>
+                    <input type="text" name="matrixWidth" className='h-10 w-1/3 bg-transparent border-2 text-center shadow-[5px_5px_0_0_rgba(208,237,87,1)]' required></input>
+                </div>
+                <div className='p-5 flex flex-row justify-between'>
+                    <label >Jumlah Sequences</label>
+                    <label >:</label>
+                </div>
+                <input type="text" name="sequenceSize" className='h-10 m-5 bg-transparent border-2 text-center shadow-[5px_5px_0_0_rgba(208,237,87,1)]' required></input>
+                <div className='p-5 flex flex-row justify-between'>
+                    <label >Ukuran Maksimal Sequences</label>
+                    <label >:</label>
+                </div>
+                <input type="text" name="maxSequence" className='h-10 m-5 bg-transparent border-2 text-center shadow-[5px_5px_0_0_rgba(208,237,87,1)]' required></input>
+                <div className='col-span-2 flex items-center justify-center p-5'>
+                <button type='submit' className='w-1/3 border-2 border-solid p-3 text-3xl shadow-[5px_5px_0_0_rgba(208,237,87,1)] hover:bg-[#d0ed57] hover:bg-opacity-10 active:scale-95 transition duration-150 ease-in-out'>Solve</button>
+                </div>
+            </form>
+            <div>
+                <a href='/file-upload'><button className="fixed top-10 right-10 border-2 border-[#d0ed57] border-solid p-2 text-l font-sans font-semibold shadow-[5px_5px_0_0_rgba(208,237,87,1)] hover:bg-[#d0ed57] hover:bg-opacity-10 active:scale-95 transition duration-150 ease-in-out">Input From File</button></a>
+            </div>
+            <div>
+                <a href='/'><button className="fixed top-10 left-10 border-2 border-[#d0ed57] border-solid p-2 text-l font-sans font-semibold shadow-[5px_5px_0_0_rgba(208,237,87,1)] hover:bg-[#d0ed57] hover:bg-opacity-10 active:scale-95 transition duration-150 ease-in-out">First Page</button></a>
+            </div>
         </div>
-        <form 
-            action="http://localhost:5000/solve" 
-            method="POST" 
-            className='grid  grid-cols-2 mt-20 text-xl font-sans font-semibold w-1/2'
-            >
-            <div className='p-5 flex flex-row justify-between'>
-                <label >Jumlah Token Unik </label>
-                <label >:</label>
+        {showResult && (
+        <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[#111] text-white p-5 rounded-lg flex flex-col items-center z-50 max-w-4xl max-h-[80vh] overflow-auto w-2/3 h-2/3">
+          <h2 className="text-xl font-bold w-full  bg-[#d0ed57] text-black text-center">Results:</h2>
+          <div className='flex flex-row justify-between w-2/3'>
+            <div className="my-5">
+              <h3 className="text-lg font-semibold">Matrix:</h3>
+              {renderMatrix(results.data.matrix)}
             </div>
-            <input type="text" name="uniqueToken" className='h-10 m-5 bg-transparent border-2 text-center shadow-[5px_5px_0_0_rgba(208,237,87,1)] p-3' required></input>
-            <div className='p-5 flex flex-row justify-between'>
-                <label >Token Unik </label>
-                <label >:</label>
+            <div className="my-5">
+              <h3 className="text-lg font-semibold">Sequences and Rewards:</h3>
+              {results.data.sequences.map((seq, index) => (
+                <div key={index} className="mt-2">
+                  <span>- Sequence: {seq.tokens.join(", ")} </span>
+                  <span className='ml-3'> | Reward: {seq.reward}</span>
+                </div>
+              ))}
             </div>
-            <input type="text" name="token" className='h-10 m-5 bg-transparent border-2 text-center shadow-[5px_5px_0_0_rgba(208,237,87,1)]' required></input>
-            <div className='p-5 flex flex-row justify-between'>
-                <label  >Ukuran Buffer </label>
-                <label >:</label>
+          </div>
+          <div className='flex flex-row justity-between w-2/3'>
+            <div className="my-5 w-2/3">
+              <h3 className="text-lg font-semibold">Best Path:</h3>
+              <div>
+                {results.bestPath.map(tokenInfo => tokenInfo.value).join(" ")}
+              </div>
             </div>
-            <input type="text" name="bufferSize" className='h-10 m-5 bg-transparent border-2 text-center shadow-[5px_5px_0_0_rgba(208,237,87,1)] 'required></input>
-            <div className='p-5 flex flex-row justify-between'>
-                <label >Ukuran Matriks </label>
-                <label >:</label>
+            <div className="my-5">
+              <h3 className="text-lg font-semibold">Max Reward:</h3>
+              <pre className="whitespace-pre-wrap">{results.maxReward}</pre>
             </div>
-            <div className='flex flex-row p-5 gap-4'>
-                <label className='mt-1' >Rows</label>
-                <input type="text" name="matrixHeight" className='h-10  w-1/3 bg-transparent border-2 text-center shadow-[5px_5px_0_0_rgba(208,237,87,1)]' required></input>
-                <label className='mt-1' >Cols</label>
-                <input type="text" name="matrixWidth" className='h-10 w-1/3 bg-transparent border-2 text-center shadow-[5px_5px_0_0_rgba(208,237,87,1)]' required></input>
+          </div>
+          <div className="my-5 w-2/3">
+            <h3 className="text-lg font-semibold">Coordinates of Best Path:</h3>
+            <div className='flex flex-col gap-3'>
+              {results.bestPath.map((tokenInfo, index) => (
+                <div key={index}>
+                  {tokenInfo.pos.y + 1}, {tokenInfo.pos.x + 1}
+                </div>
+              ))}
             </div>
-            <div className='p-5 flex flex-row justify-between'>
-                <label >Jumlah Sequences</label>
-                <label >:</label>
-            </div>
-            <input type="text" name="sequenceSize" className='h-10 m-5 bg-transparent border-2 text-center shadow-[5px_5px_0_0_rgba(208,237,87,1)]' required></input>
-            <div className='p-5 flex flex-row justify-between'>
-                <label >Ukuran Maksimal Sequences</label>
-                <label >:</label>
-            </div>
-            <input type="text" name="maxSequence" className='h-10 m-5 bg-transparent border-2 text-center shadow-[5px_5px_0_0_rgba(208,237,87,1)]' required></input>
-            <div className='col-span-2 flex items-center justify-center p-5'>
-            <button type='submit' className='w-1/3 border-2 border-solid p-3 text-3xl shadow-[5px_5px_0_0_rgba(208,237,87,1)] hover:bg-[#d0ed57] hover:bg-opacity-10 active:scale-95 transition duration-150 ease-in-out'>Solve</button>
-            </div>
-        </form>
+          </div>
+          <div>
+          <button onClick={handleResult} className="border-2 border-[#d0ed57] border-solid p-4 text-l font-sans font-semibold shadow-[5px_5px_0_0_rgba(208,237,87,1)] hover:bg-[#d0ed57] hover:bg-opacity-10 active:scale-95 transition duration-150 ease-in-out">Close</button>
+          </div>
+        </div>
+    )}
+        
     </div>
   )
 }
