27a370b8406a8b369da1e0e481be3adf7f72b95f
Benardo07 <117990908+Benardo07@users.noreply.github.com>
2024-02-12 02:58:13 +0700

    feat : add pop up modal

--- src/frontend/src/components/file.js
+++ src/frontend/src/components/file.js
@@ -1,7 +1,91 @@
+// import React, { useState, useRef } from 'react';
+
+// function File() {
+//   const [selectedFile, setSelectedFile] = useState(null);
+//   const fileInputRef = useRef();
+
+//   const handleDragOver = (event) => {
+//     event.preventDefault(); // Prevent default behavior (Prevent file from being opened)
+//   };
+
+//   const handleDrop = (event) => {
+//     event.preventDefault();
+//     if (event.dataTransfer.files && event.dataTransfer.files[0]) {
+//       setSelectedFile(event.dataTransfer.files[0]);
+//       // If you want to support uploading the dropped file, you can do so here
+//     }
+//   };
+
+//   const handleFileChange = (event) => {
+//     if (event.target.files && event.target.files[0]) {
+//       setSelectedFile(event.target.files[0]);
+//     }
+//   };
+
+//   const handleClick = () => {
+//     // Programmatically click the hidden file input
+//     fileInputRef.current.click();
+//   };
+
+//   const handleSolveClick = async () => {
+//     if (!selectedFile) {
+//       alert('Please select a file first.');
+//       return;
+//     }
+
+//     const formData = new FormData();
+//     formData.append('file', selectedFile);
+
+//     try {
+//       const response = await fetch('/solveFile', {
+//         method: 'POST',
+//         body: formData,
+//       });
+//       const data = await response.json();
+//       console.log(data); // Process the response data as needed
+//     } catch (error) {
+//       console.error('Error uploading file:', error);
+//     }
+//   };
+//   return (
+//     <div className='w-screen h-screen flex flex-col items-center text-[#d0ed57]'>
+//       <div className="text-6xl font-sans font-bold text-shadow mt-20 text-center">
+//         Cyberpunk 2077 Breach Protocol
+//       </div>
+//       <div className="mt-10">
+//         <div 
+//           className="border-2 border-dashed border-[#d0ed57] rounded-lg p-10 w-96 text-center cursor-pointer"
+//           onClick={handleClick}
+//           onDragOver={handleDragOver}
+//           onDrop={handleDrop}
+//         >
+//           Drag and drop a file here, or click to select a file.
+//         </div>
+//         <input
+//           ref={fileInputRef}
+//           type="file"
+//           onChange={handleFileChange}
+//           className="hidden"
+//         />
+//         {selectedFile && (
+//           <p className="mt-4 text-center">Selected file: {selectedFile.name}</p>
+//         )}
+//       </div>
+//       <button onClick={handleSolveClick}  className='w-40 mt-5 border-2 border-solid p-3 text-3xl shadow-[5px_5px_0_0_rgba(208,237,87,1)] hover:bg-[#d0ed57] hover:bg-opacity-10 active:scale-95 transition duration-150 ease-in-out'>Solve</button>
+
+//     </div>
+//   );
+// }
+
+// export default File;
+
 import React, { useState, useRef } from 'react';
 
 function File() {
   const [selectedFile, setSelectedFile] = useState(null);
+
+  const [showResult, setShowResult] = useState(false);
+  const [results, setResults] = useState({});
   const fileInputRef = useRef();
 
   const handleDragOver = (event) => {
@@ -12,7 +96,6 @@ function File() {
     event.preventDefault();
     if (event.dataTransfer.files && event.dataTransfer.files[0]) {
       setSelectedFile(event.dataTransfer.files[0]);
-      // If you want to support uploading the dropped file, you can do so here
     }
   };
 
@@ -23,18 +106,21 @@ function File() {
   };
 
   const handleClick = () => {
-    // Programmatically click the hidden file input
     fileInputRef.current.click();
   };
 
+
+
   const handleSolveClick = async () => {
-    if (!selectedFile) {
-      alert('Please select a file first.');
+
+    if (!selectedFile ) {
+      alert('Please select a file and enter a start point.');
       return;
     }
 
     const formData = new FormData();
     formData.append('file', selectedFile);
+
 
     try {
       const response = await fetch('/solveFile', {
@@ -42,11 +128,34 @@ function File() {
         body: formData,
       });
       const data = await response.json();
-      console.log(data); // Process the response data as needed
+
+      setResults(data);
+      setShowResult(true);
     } catch (error) {
       console.error('Error uploading file:', error);
     }
   };
+  const renderMatrix = (matrix) => {
+    return (
+      <table style={{ margin: "auto", borderCollapse: "collapse" }}>
+        <tbody>
+          {matrix.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {row.map((cell, cellIndex) => (
+                <td key={cellIndex} style={{ border: "1px solid #d0ed57", padding: "5px", textAlign: "center" }}>
+                  {cell}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  };
+  const handleResult = () => {
+    setShowResult(false);
+  }
+
   return (
     <div className='w-screen h-screen flex flex-col items-center text-[#d0ed57]'>
       <div className="text-6xl font-sans font-bold text-shadow mt-20 text-center">
@@ -71,10 +180,64 @@ function File() {
           <p className="mt-4 text-center">Selected file: {selectedFile.name}</p>
         )}
       </div>
-      <button onClick={handleSolveClick}  className='w-40 mt-5 border-2 border-solid p-3 text-3xl shadow-[5px_5px_0_0_rgba(208,237,87,1)] hover:bg-[#d0ed57] hover:bg-opacity-10 active:scale-95 transition duration-150 ease-in-out'>Solve</button>
-
+      <button onClick={handleSolveClick} className='w-40 mt-5 border-2 border-solid p-3 text-3xl shadow-[5px_5px_0_0_rgba(208,237,87,1)] hover:bg-[#d0ed57] hover:bg-opacity-10 active:scale-95 transition duration-150 ease-in-out'>Solve</button>
+      
+      <div>
+          <a href='/manual'><button className="fixed top-10 right-10 border-2 border-[#d0ed57] border-solid p-2 text-l font-sans font-semibold shadow-[5px_5px_0_0_rgba(208,237,87,1)] hover:bg-[#d0ed57] hover:bg-opacity-10 active:scale-95 transition duration-150 ease-in-out">Input From File</button></a>
+      </div>
+      <div>
+          <a href='/'><button className="fixed top-10 left-10 border-2 border-[#d0ed57] border-solid p-2 text-l font-sans font-semibold shadow-[5px_5px_0_0_rgba(208,237,87,1)] hover:bg-[#d0ed57] hover:bg-opacity-10 active:scale-95 transition duration-150 ease-in-out">First Page</button></a>
+      </div>
+      {showResult && (
+        <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[#111] text-white p-5 rounded-lg flex flex-col items-center z-50 max-w-4xl max-h-[80vh] overflow-auto w-2/3 h-2/3">
+          <h2 className="text-xl font-bold w-full  bg-[#d0ed57] text-black text-center">Results:</h2>
+          <div className='flex flex-row justify-between w-2/3'>
+            <div className="my-5">
+              <h3 className="text-lg font-semibold">Matrix:</h3>
+              {renderMatrix(results.data.matrix)}
+            </div>
+            <div className="my-5">
+              <h3 className="text-lg font-semibold">Sequences and Rewards:</h3>
+              {results.data.sequences.map((seq, index) => (
+                <div key={index} className="mt-2">
+                  <span>- Sequence: {seq.tokens.join(", ")} </span>
+                  <span className='ml-3'> | Reward: {seq.reward}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className='flex flex-row justity-between w-2/3'>
+            <div className="my-5 w-2/3">
+              <h3 className="text-lg font-semibold">Best Path:</h3>
+              <div>
+                {results.bestPath.map(tokenInfo => tokenInfo.value).join(" ")}
+              </div>
+            </div>
+            <div className="my-5">
+              <h3 className="text-lg font-semibold">Max Reward:</h3>
+              <pre className="whitespace-pre-wrap">{results.maxReward}</pre>
+            </div>
+          </div>
+          <div className="my-5 w-2/3">
+            <h3 className="text-lg font-semibold">Coordinates of Best Path:</h3>
+            <div className='flex flex-col gap-3'>
+              {results.bestPath.map((tokenInfo, index) => (
+                <div key={index}>
+                  {tokenInfo.pos.y + 1}, {tokenInfo.pos.x + 1}
+                </div>
+              ))}
+            </div>
+          </div>
+          <div>
+          <button onClick={handleResult} className="border-2 border-[#d0ed57] border-solid p-4 text-l font-sans font-semibold shadow-[5px_5px_0_0_rgba(208,237,87,1)] hover:bg-[#d0ed57] hover:bg-opacity-10 active:scale-95 transition duration-150 ease-in-out">Close</button>
+          </div>
+        </div>
+    )}
+      
+      
     </div>
   );
 }
 
 export default File;
+
