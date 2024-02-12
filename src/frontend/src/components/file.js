ca39d49685850330267f6359b930b41914f0a970
Benardo07 <117990908+Benardo07@users.noreply.github.com>
2024-02-13 04:36:50 +0700

    add loading, wrapping

--- src/frontend/src/components/file.js
+++ src/frontend/src/components/file.js
@@ -1,89 +1,10 @@
-// import React, { useState, useRef } from 'react';
 
-// function File() {
-//   const [selectedFile, setSelectedFile] = useState(null);
-//   const fileInputRef = useRef();
-
-//   const handleDragOver = (event) => {
-//     event.preventDefault(); // Prevent default behavior (Prevent file from being opened)
-//   };
-
-//   const handleDrop = (event) => {
-//     event.preventDefault();
-//     if (event.dataTransfer.files && event.dataTransfer.files[0]) {
-//       setSelectedFile(event.dataTransfer.files[0]);
-//       // If you want to support uploading the dropped file, you can do so here
-//     }
-//   };
-
-//   const handleFileChange = (event) => {
-//     if (event.target.files && event.target.files[0]) {
-//       setSelectedFile(event.target.files[0]);
-//     }
-//   };
-
-//   const handleClick = () => {
-//     // Programmatically click the hidden file input
-//     fileInputRef.current.click();
-//   };
-
-//   const handleSolveClick = async () => {
-//     if (!selectedFile) {
-//       alert('Please select a file first.');
-//       return;
-//     }
-
-//     const formData = new FormData();
-//     formData.append('file', selectedFile);
-
-//     try {
-//       const response = await fetch('/solveFile', {
-//         method: 'POST',
-//         body: formData,
-//       });
-//       const data = await response.json();
-//       console.log(data); // Process the response data as needed
-//     } catch (error) {
-//       console.error('Error uploading file:', error);
-//     }
-//   };
-//   return (
-//     <div className='w-screen h-screen flex flex-col items-center text-[#d0ed57]'>
-//       <div className="text-6xl font-sans font-bold text-shadow mt-20 text-center">
-//         Cyberpunk 2077 Breach Protocol
-//       </div>
-//       <div className="mt-10">
-//         <div 
-//           className="border-2 border-dashed border-[#d0ed57] rounded-lg p-10 w-96 text-center cursor-pointer"
-//           onClick={handleClick}
-//           onDragOver={handleDragOver}
-//           onDrop={handleDrop}
-//         >
-//           Drag and drop a file here, or click to select a file.
-//         </div>
-//         <input
-//           ref={fileInputRef}
-//           type="file"
-//           onChange={handleFileChange}
-//           className="hidden"
-//         />
-//         {selectedFile && (
-//           <p className="mt-4 text-center">Selected file: {selectedFile.name}</p>
-//         )}
-//       </div>
-//       <button onClick={handleSolveClick}  className='w-40 mt-5 border-2 border-solid p-3 text-3xl shadow-[5px_5px_0_0_rgba(208,237,87,1)] hover:bg-[#d0ed57] hover:bg-opacity-10 active:scale-95 transition duration-150 ease-in-out'>Solve</button>
-
-//     </div>
-//   );
-// }
-
-// export default File;
 
 import React, { useState, useRef } from 'react';
 
 function File() {
   const [selectedFile, setSelectedFile] = useState(null);
-
+  const [isLoading, setIsLoading] = useState(false);
   const [showResult, setShowResult] = useState(false);
   const [results, setResults] = useState({});
   const fileInputRef = useRef();
@@ -96,12 +17,14 @@ function File() {
     event.preventDefault();
     if (event.dataTransfer.files && event.dataTransfer.files[0]) {
       setSelectedFile(event.dataTransfer.files[0]);
+      fileInputRef.current.value = '';
     }
   };
 
   const handleFileChange = (event) => {
     if (event.target.files && event.target.files[0]) {
       setSelectedFile(event.target.files[0]);
+      event.target.value = '';
     }
   };
 
@@ -114,25 +37,34 @@ function File() {
   const handleSolveClick = async () => {
 
     if (!selectedFile ) {
-      alert('Please select a file and enter a start point.');
+      alert('Please select a file ');
       return;
     }
 
     const formData = new FormData();
     formData.append('file', selectedFile);
-
+    setIsLoading(true);
 
     try {
       const response = await fetch('/solveFile', {
         method: 'POST',
         body: formData,
       });
+
+      if (response.status === 400) {
+        const errorData = await response.json(); // Assuming the backend sends a JSON response with an error message
+        alert(`Error: ${errorData.message}`); // Display the error message from the backend
+        setIsLoading(false); // Stop loading since we've handled the error
+        return; // Exit the function early to avoid further processing
+    }
       const data = await response.json();
 
       setResults(data);
       setShowResult(true);
     } catch (error) {
       console.error('Error uploading file:', error);
+    }finally {
+      setIsLoading(false); // Stop loading regardless of the outcome
     }
   };
   const renderMatrix = (matrix) => {
@@ -154,6 +86,7 @@ function File() {
   };
   const handleResult = () => {
     setShowResult(false);
+    setSelectedFile(null);
   }
 
   const handleDownloadResult = () => {
@@ -169,7 +102,7 @@ function File() {
       content += `${tokenInfo.pos.x + 1}, ${tokenInfo.pos.y + 1}\n`;
     });
     content += `${results.executionTime} ms\n`;
-    
+
     // Convert the content to a Blob object
     const blob = new Blob([content], { type: 'text/plain' });
 
@@ -192,6 +125,11 @@ function File() {
 
   return (
     <div className='w-screen h-screen flex flex-col items-center text-[#d0ed57]'>
+      {isLoading && (
+        <div className="fixed w-20 h-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-50 text-white p-5 rounded-lg flex flex-col items-center z-50">
+          <p>Loading...</p>
+        </div>
+      )}
       <div className={`flex flex-col items-center w-full h-full ${showResult ? 'filter blur-sm' : ''}`}>
         <div className="text-6xl font-sans font-bold text-shadow mt-20 text-center">
           Cyberpunk 2077 Breach Protocol
@@ -227,7 +165,7 @@ function File() {
       {showResult && (
         <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[#111] text-white p-5 rounded-lg flex flex-col items-center z-50 max-w-4xl max-h-[80vh] overflow-auto w-2/3 h-2/3">
           <h2 className="text-xl font-bold w-full  bg-[#d0ed57] text-black text-center">Results:</h2>
-          <div className='flex flex-row justify-between w-2/3'>
+          <div className='flex flex-row justify-between w-2/3 flex-wrap'>
             <div className="my-5">
               <h3 className="text-lg font-semibold">Matrix:</h3>
               {renderMatrix(results.data.matrix)}
@@ -243,12 +181,22 @@ function File() {
             </div>
           </div>
           <div className='flex flex-row justity-between w-2/3'>
-            <div className="my-5 w-2/3">
-              <h3 className="text-lg font-semibold">Best Path:</h3>
-              <div>
-                {results.bestPath.map(tokenInfo => tokenInfo.value).join(" ")}
+          {results.maxReward > 0 ? (
+            <div className='flex flex-row justify-between w-2/3'>
+              <div className="my-5 w-2/3">
+                <h3 className="text-lg font-semibold">Best Path:</h3>
+                <div>{results.bestPath.map(tokenInfo => tokenInfo.value).join(" ")}</div>
+              </div>
+              <div className="my-5">
+                <h3 className="text-lg font-semibold">Max Reward:</h3>
+                <pre className="whitespace-pre-wrap">{results.maxReward}</pre>
               </div>
             </div>
+          ) : (
+            <div className="my-5 w-full text-center">
+              <h3 className="text-lg font-semibold">No Solution</h3>
+            </div>
+          )}
             <div className="my-5">
               <h3 className="text-lg font-semibold">Max Reward:</h3>
               <pre className="whitespace-pre-wrap">{results.maxReward}</pre>
